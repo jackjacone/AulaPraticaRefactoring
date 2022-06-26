841daac50ee358287f35db38b3e98ffe52508cb5
Lucas Silva <75103788+jackjacone@users.noreply.github.com>
2022-06-26 01:10:10 +0000

    Commit 12

--- Movie.java
+++ Movie.java
@@ -33,26 +33,8 @@ public class Movie {
       }
    }
 
-  public double getCharge(int DaysRented){
-    //determine amounts for each line
-    double thisAmount = 0;
-    switch (this.getPriceCode()) {
-        case Movie.REGULAR:
-          thisAmount += 2;
-          if (DaysRented > 2)
-              thisAmount += (DaysRented - 2) * 1.5;
-          break;
-        case Movie.NEW_RELEASE:
-          thisAmount += DaysRented * 3;
-          break;
-        case Movie.CHILDRENS:
-          thisAmount += 1.5;
-          if (DaysRented > 3)
-              thisAmount += (DaysRented - 3) * 1.5;
-            break;
-    }
-
-    return thisAmount;
+  public double getCharge(int daysRented) {
+         return _price.getCharge(daysRented);
 
   }
   
--- Price.java
+++ Price.java
@@ -1,3 +1,24 @@
 public abstract class Price {
    public abstract int getPriceCode();
+
+  public double getCharge(int DaysRented){
+
+    double thisAmount = 0;
+    switch (this.getPriceCode()) {
+        case Movie.REGULAR:
+          thisAmount += 2;
+          if (DaysRented > 2)
+              thisAmount += (DaysRented - 2) * 1.5;
+          break;
+        case Movie.NEW_RELEASE:
+          thisAmount += DaysRented * 3;
+          break;
+        case Movie.CHILDRENS:
+          thisAmount += 1.5;
+          if (DaysRented > 3)
+              thisAmount += (DaysRented - 3) * 1.5;
+            break;
+    }
+    return thisAmount;
+  }
 } 
