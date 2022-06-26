445a633568e9536d71e5b38b1e9509832fbeeb94
Lucas Silva <75103788+jackjacone@users.noreply.github.com>
2022-06-26 00:50:09 +0000

    Commit 9

--- Movie.java
+++ Movie.java
@@ -7,6 +7,29 @@ public class Movie {
   private String _title;
   private int _priceCode;
 
+  public double getCharge(int DaysRented){
+    //determine amounts for each line
+    double thisAmount = 0;
+    switch (this.getPriceCode()) {
+        case Movie.REGULAR:
+          thisAmount += 2;
+          if (DaysRented > 2)
+              thisAmount += (DaysRented - 2) * 1.5;
+          break;
+        case Movie.NEW_RELEASE:
+          thisAmount += DaysRented * 3;
+          break;
+        case Movie.CHILDRENS:
+          thisAmount += 1.5;
+          if (DaysRented > 3)
+              thisAmount += (DaysRented - 3) * 1.5;
+            break;
+    }
+
+    return thisAmount;
+
+  }
+
   public Movie(String title, int priceCode) {
       _title = title;
       _priceCode = priceCode;
