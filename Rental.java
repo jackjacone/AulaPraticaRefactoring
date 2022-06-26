6652e91f903f3cf00aebe22d8b5a32568377765a
Lucas Silva <75103788+jackjacone@users.noreply.github.com>
2022-06-26 00:16:38 +0000

    Commit 4

--- Rental.java
+++ Rental.java
@@ -3,6 +3,28 @@ public class Rental {
    private Movie _movie;
    private int _daysRented;
 
+  public double getCharge(){
+    //determine amounts for each line
+        double thisAmount = 0;
+        switch (this.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+              thisAmount += 2;
+              if (this.getDaysRented() > 2)
+                  thisAmount += (this.getDaysRented() - 2) * 1.5;
+              break;
+            case Movie.NEW_RELEASE:
+              thisAmount += this.getDaysRented() * 3;
+              break;
+            case Movie.CHILDRENS:
+              thisAmount += 1.5;
+              if (this.getDaysRented() > 3)
+                  thisAmount += (this.getDaysRented() - 3) * 1.5;
+                break;
+    }
+
+    return thisAmount;
+   }
+
    public Rental(Movie movie, int daysRented) {
       _movie = movie;
       _daysRented = daysRented;
